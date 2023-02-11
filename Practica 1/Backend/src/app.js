956b663f49261b0393a97baa0ef183e39664d3f2
Eduado Catalán <marvineduardocv12@gmail.com>
2023-02-11 17:54:15 -0600

    Configuración y conexión a la base de datos.

--- Practica 1/Backend/src/app.js
+++ Practica 1/Backend/src/app.js
@@ -1,11 +1,18 @@
 import express from "express";
 import morgan from "morgan";
-
+import config from "./config";
+const mysql = require("mysql")
 const app = express();
 
 app.set("port",4010);
 
 // Middlewares
 app.use(morgan("dev"));
+
+/**
+ * Creamos pool conection para evitar crear muchas conexiones
+ * @type {Pool}
+ */
+app.locals.mysqlConnectionPool = mysql.createPool(config.mysql);
 
 export default  app;
--- Practica 1/Backend/src/config.js
+++ Practica 1/Backend/src/config.js
@@ -1,2 +1,13 @@
 import { config } from "dotenv";
-
+config({path:`./.env.${process.env.NODE_ENV}`});
+export default {
+    mysql:{
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        port: process.env.PORT,
+        database: process.env.DATABASE_NAME,
+        connectionLimit: 10,
+        acquireTimeout: 300
+    }
+}
