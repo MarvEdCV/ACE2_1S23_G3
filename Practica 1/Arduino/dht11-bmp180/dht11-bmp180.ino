0dbcdfcfe2b76785717a7561b4430abc4fc2002b
Reyes06 <eddyarnoldo06@gmail.com>
2023-02-17 23:32:49 -0600

    lectura mas precisa del sensor de direccion de viento

--- Practica 1/Arduino/dht11-bmp180/dht11-bmp180.ino
+++ Practica 1/Arduino/dht11-bmp180/dht11-bmp180.ino
@@ -13,7 +13,8 @@ int pin = A0;
 int pinN = 22;
 int pinE = 24;
 int pinS = 26;
-int pinO = 27;
+int pinO = 28;
+float grados = 0;
 
 void setup() {
   // Inicializamos comunicación serie
@@ -44,18 +45,6 @@ void loop() {
   float voltaje = analogRead(pin);
   float velocity = voltaje * 0.190;
 
-  float grados = 0;
-  if(digitalRead(pinN)) {
-    grados = 0;
-  } else if(digitalRead(pinE)) {
-    grados = 90;
-  } else if(digitalRead(pinS)) {
-    grados = 180;
-  } else if(digitalRead(pinO)) {
-    grados = 270;
-  }
-
-
   float punto_rocio = pow(humedad_relativa / 100, 0.125) * (112 + 0.9 * temperatura) + (0.1 * temperatura) - 112;
   float humedad_absoluta = (humedad_relativa/100) * 6.112 * exp((17.67 * temperatura)/(temperatura + 243.5)) * 2.1674 / (273.15 + temperatura);
 
@@ -66,6 +55,21 @@ void loop() {
   //Viento
   Serial.println("viento,"+String(velocity)+","+String(grados));
   
-  delay(5000);
+  //delay(5000);
+
+  int contador = 0;
+  while(contador < 1000){
+    if(digitalRead(pinN)==0) {
+      grados = 0;
+    } else if(digitalRead(pinE)==0) {
+      grados = 90;
+    } else if(digitalRead(pinS)==0) {
+      grados = 180;
+    } else if(digitalRead(pinO)==0) {
+      grados = 270;
+    }
+    contador++;
+    delay(5);
+  }
  
 }
