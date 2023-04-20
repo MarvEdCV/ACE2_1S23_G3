aa8f6f959a0d7bb712c17d7e3ac1021dbfd4b4ec
carlos <carlosorantes77@gmail.com>
2023-04-20 01:00:03 -0600

    parte inicial de la app ya funciona con ruteo correctamente

--- Proyecto 2/frontend/riego-domestico/src/app/app-routing.module.ts
+++ Proyecto 2/frontend/riego-domestico/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './dashboard/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {path:"dashboard", component: LayoutComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
