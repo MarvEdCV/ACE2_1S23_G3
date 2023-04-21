31b3fdec877ed95ac2761983b06ee52879f0d7dc
carlos <carlosorantes77@gmail.com>
2023-04-21 16:11:05 -0600

    ya se muestra la primer grafica, pero aun no esta pulida al 100%

--- Proyecto 2/frontend/riego-domestico/src/app/app.module.ts
+++ Proyecto 2/frontend/riego-domestico/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './dashboard/layout/layout.component';
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { LayoutComponent } from './dashboard/layout/layout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
